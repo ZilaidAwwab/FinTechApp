e77a8ad0e3c8538b51ca0e73abf8e1e3a1c7d23c
ZilaidAwwab <zilaidawwab786@gmail.com>
2025-02-11 15:55:53 +0500

    Initial Commit

---
+++ src/main/java/com/zilaidawwab/fintechapp/FinTechAppApplication.java
@@ -0,0 +1,13 @@
+package com.zilaidawwab.fintechapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinTechAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinTechAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zilaidawwab/fintechapp/FinTechAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zilaidawwab.fintechapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinTechAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
